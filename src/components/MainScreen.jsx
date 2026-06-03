9e9b5273e777d7e3cd187a8d53d90d3bba8cc56b
beetauri <68471315+beetauri@users.noreply.github.com>
2026-06-04 01:10:25 +0300

    Revert "add component library drawer"
    This reverts commit 4151beeb62ab07892cdfba33a899f6c5083154af.

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -14,7 +14,6 @@ import {
   UserRound as LucideUserIcon,
   Zap as LucideFlashIcon
 } from 'lucide-react';
-import ComponentLibraryDrawer from './ComponentLibraryDrawer';
 import HistoryScreen from './HistoryScreen';
 import { db, storage, auth, functions, doc, onSnapshot, updateDoc, updateProfile, ref, uploadBytes, getDownloadURL, signOut, collection, addDoc, query, orderBy, httpsCallable } from '../firebase';
 import { trackEvent } from '../analytics';
@@ -1209,7 +1208,8 @@ function ProfileView({
       </div>
 
       <div className="profile-link-row">
-        <ComponentLibraryDrawer />
+        <a href="#privacy" onClick={(e) => e.preventDefault()}>Privacy Notice</a>
+        <a href="#terms" onClick={(e) => e.preventDefault()}>Terms of Use</a>
       </div>
 
       <div className="profile-danger-zone">
