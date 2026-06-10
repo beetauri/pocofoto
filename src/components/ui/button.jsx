f339e29770d90662afcc1afbdec9bdae33b2ed11
beetauri <68471315+beetauri@users.noreply.github.com>
2026-06-10 04:51:59 +0300

    chore: align button.jsx with shadcn registry

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,3 +1,4 @@
+import * as React from "react"
 import { cva } from "class-variance-authority";
 import { Slot } from "radix-ui"
 
@@ -57,4 +58,4 @@ function Button({
   );
 }
 
-export { Button }
+export { Button, buttonVariants }
