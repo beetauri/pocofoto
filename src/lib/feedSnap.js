2e3e6758b838d810373248ba6672fc4386260907
beetauri <68471315+beetauri@users.noreply.github.com>
2026-06-23 14:58:40 +0300

    Revert "fix feed scroll settling"
    This reverts commit 01fb410a3150a318452945a80750f266c242b70a.

--- src/lib/feedSnap.js
+++
@@ -1,7 +0,0 @@
-export function getNearestFeedSnapTop(scrollTop, slideTops) {
-  if (!slideTops.length) return scrollTop;
-
-  return slideTops.reduce((nearest, current) => {
-    return Math.abs(current - scrollTop) < Math.abs(nearest - scrollTop) ? current : nearest;
-  }, slideTops[0]);
-}
